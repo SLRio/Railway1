db0fa3c86a5c1ca747e522f920478dd559a9b640
Rio <121921604+SLRio@users.noreply.github.com>
2025-02-08 13:27:03 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://Rio:RioAstal1234@rio.kh2t4sq.mongodb.net/?retryWrites=true&w=majority', {
+        await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
         });
-        console.log('MongoDB connected to maheshdb...');
+        console.log('✅ MongoDB connected to maheshdb...');
     } catch (err) {
-        console.error(err.message);
+        console.error('❌ MongoDB connection error:', err.message);
         process.exit(1);
     }
 };
